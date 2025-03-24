8938263b28fb8b02d4d4ce9782b1c404fc1cefa7
rtfmbrah <bosna@hotmail.com>
2025-03-24 14:59:20 +0100

    Chapter 3

--- chapter3/src/main.rs
+++ chapter3/src/main.rs
@@ -1,3 +1,82 @@
+use std::io;
+
 fn main() {
-    //TODO Rustbook chapter 3 
+    vars_and_mut();
+    arrays();
+    return_values();
+    control_flow();
+    loop_returns();
+    for_loop();
+}
+
+fn vars_and_mut() {
+    let x = 5; 
+
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+
+    println!("The value of x is: {x}");
+}
+
+fn arrays() {
+    let a = [1, 2, 3, 4, 5];
+
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+    
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!("The value of the element at index {index} is: {element}");
+}
+
+fn return_values() {
+    let x = plus_one(5);
+
+    println!("The value of x is: {x}");
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
+}
+
+fn control_flow() {
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+
+    println!("The value of number is: {number}");
+}
+
+fn loop_returns() {
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {result}");
+}
+
+fn for_loop() {
+    for number in (1..4).rev() {
+        println!("{number}!");
+    }
+    println!("LIFTOFF!!!");
 }
---
+++ chapter4/src/main.rs
@@ -0,0 +1,3 @@
+fn main() {
+    //TODO: Chapter 4
+}
